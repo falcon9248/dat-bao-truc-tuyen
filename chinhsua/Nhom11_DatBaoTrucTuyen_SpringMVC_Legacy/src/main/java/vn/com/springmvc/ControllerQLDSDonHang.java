89b7caafc3e55625f26e11ec4a6dc7e298e3ede2
vinhnhat <vinhnhat1512@gmail.com>
2018-05-04 15:53:01 +0700

    vinh
    chinhsua

---
+++ chinhsua/Nhom11_DatBaoTrucTuyen_SpringMVC_Legacy/src/main/java/vn/com/springmvc/ControllerQLDSDonHang.java
@@ -0,0 +1,30 @@
+package vn.com.springmvc;
+
+/**
+ * @author PuppyDRyuki
+ * @version 1.0
+ * @created 18-Apr-2018 23:30:17
+ */
+public class ControllerQLDSDonHang {
+
+	public ControllerQLDSDonHang(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public int duyetDonHang(){
+		return 0;
+	}
+
+	public int huyDonHang(){
+		return 0;
+	}
+
+	public int suaDonHang(){
+		return 0;
+	}
+
+}
---
+++ chinhsua/Nhom11_DatBaoTrucTuyen_SpringMVC_Legacy/src/main/java/vn/com/springmvc/beans/HoaDon.java
@@ -0,0 +1,26 @@
+package vn.com.springmvc.beans;
+
+import java.sql.Date;
+
+/**
+ * @author FALCON
+ * @version 1.0
+ * @created 18-Apr-2018 23:30:32
+ */
+public class HoaDon {
+
+	private String maHoaDon;
+	private int maTaiKhoan;
+	private Date ngayLapHD;
+	private int tongTien;
+	public ChiTietHoaDon m_ChiTietHoaDon;
+
+	public HoaDon(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
